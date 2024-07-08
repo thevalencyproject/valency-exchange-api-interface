e73a917d3bbab01a8311bece55e48ad1b77e2cec
Isaac Ljubic <134184959+isaacljubic@users.noreply.github.com>
2024-07-08 13:10:57 +1000

    Added Assets Owned Return

--- sign-in.js
+++ sign-in.js
@@ -108,5 +108,15 @@ function handleSignInResponse(response) {
 function parseSessionKey(response) {
     const sessionKey = response.substring(2, 34);
     const clientIndex = response.substring(34, 38);
-    return { sessionKey, clientIndex };
+    
+    // Number of assets owned - 8 byte per asset amount, 2 byte per asset index (asset index, then asset amount, repeating)
+    // First 2 bytes are number of assets indexes that are owned by the customer
+    const numOfAssetIndex = response.subString(38, 40);
+    const assetsOwned = [];                                 // Contains the asset names and respspective amounts
+    for(i = 0; i < numOfAssetIndex; i = i + 2) {
+        assetsOwned[i] = response.substring((i * 10) + 40, (i * 10) + 42);
+        assetsOwned[i + 1] = response.substring((i * 10) + 42, (i * 10) + 50);
+    }
+    
+    return { sessionKey, clientIndex, assetsOwned };
 }
